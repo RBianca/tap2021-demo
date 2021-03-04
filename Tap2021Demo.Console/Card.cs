920620a7ee401218b0cc4c5eef5ecd70a9535cdc
RBianca <biancasorinarusu@yahoo.ro>
2021-03-04 19:09:31 +0200

    Added Card class

---
+++ Tap2021Demo.Console/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tap2021Demo.Console
+{
+    public class Card
+    {
+        public Card(string cardHolder)
+        {
+            Holder = cardHolder;
+            Accounts = new List<Account>();
+        }
+        public string Holder { get; private set; }
+        public IEnumerable<Account> Accounts { get; private set; }
+
+        internal void AddAcount(SavingsAccount account)
+        {
+            (Accounts as List<Account>).Add(account);
+        }
+    }
+}
