4fe4ea625d287dde47cff419e7b023d68aca9eab
RBianca <biancasorinarusu@yahoo.ro>
2021-03-04 17:04:25 +0200

    Moved classes to own files

---
+++ Tap2021Demo.Console/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tap2021Demo.Console
+{
+    public abstract class Account
+    {
+        public decimal Balance { get; private set; } //decimal in loc de double 
+        /*
+         asta poate fi scrisa si asa:
+        public decimal balance;
+        public decimal GetBalance()
+        { return balance;}
+        public void SetBalance(decimal value)
+        {balance=value;}
+        */
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+        public decimal Withdraw(decimal amount)
+        {
+            var fee = CalculateWithdrawalFee(amount);
+            amount += fee;
+            if (Balance < amount)
+            {
+                throw new InvalidOperationException("Insufficient funds!");
+            }
+            this.Balance -= amount;
+            return amount;
+        }
+        protected abstract decimal CalculateWithdrawalFee(decimal amount);
+    }
+}
--- Tap2021Demo.Console/Program.cs
+++ Tap2021Demo.Console/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Tap2021Demo.Console
 {
+
+
+    public class AtmSingleton
+    {
+        private readonly DebitAccount _debitAccount;
+        private static readonly AtmSingleton _instance=new AtmSingleton();
+        private AtmSingleton()
+        {
+            _debitAccount = new DebitAccount();
+        }
+        public static AtmSingleton Instance => _instance;
+
+        public DebitAccount DebitAccount => _debitAccount;
+        
+
+    }
     class Program
     {
+        private const int Amount = 500;
         static void Main(string[] args)
         {
-           System.Console.WriteLine("Hello World!");
+            AtmSingleton.Instance.DebitAccount.Deposit(100);
+            var account = new SavingsAccount(); // am creat o instanta 
+            account.Deposit(200); //depun bani 
+            System.Console.WriteLine($"Disponibil { account.Balance} RON");
+            try
+            {
+                account.Withdraw(100);
+
+            }
+            catch(InvalidOperationException)
+            {
+                System.Console.WriteLine("Fonduri insuficiente");
+            }
+            System.Console.WriteLine($"Disponibil {account.Balance} RON");
+
+
         }
     }
 }
