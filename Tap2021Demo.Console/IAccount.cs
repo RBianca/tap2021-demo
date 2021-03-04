1b7000263228e7f933a47c58751f08f38d06fc0b
RBianca <biancasorinarusu@yahoo.ro>
2021-03-04 17:22:38 +0200

    Added IAccount interface

---
+++ Tap2021Demo.Console/IAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace Tap2021Demo.Console
+{
+    public interface IAccount
+    {
+        decimal Balance { get; }
+
+        void Deposit(decimal amount);
+        decimal Withdraw(decimal amount);
+    }
+}
